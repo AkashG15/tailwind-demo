188b72f50825a883ec82ba203ae7e9e0a652b6e1
AkashG15 <7875987176ag@gmail.com>
2023-06-05 12:30:24 +0530

    1st commit

---
+++ src/Button/Button.tsx
@@ -0,0 +1,20 @@
+export const Button:React.FC<{
+    label:string;
+    }>=({label})=>{ 
+    
+        return(
+            <button className="w-32 bg-orange-400 "> No dynamic lable </button>
+        
+        );
+    }
+
+    export const Buttonx:React.FC<{
+        labelx:string;
+        }>=({labelx})=>{ 
+        
+            return(
+                <button className="w-32 bg-gray-400 ">{labelx} </button>
+            
+            );
+        }
+    
